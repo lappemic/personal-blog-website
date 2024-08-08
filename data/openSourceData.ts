bb06e2c6c9b39c1afa24cac37aefbc75a0f09fed
Michael Scheiwiller <mi.lappert@gmail.com>
2024-08-08 10:19:10 +0200

    os: udpate my os experience

--- data/openSourceData.ts
+++ data/openSourceData.ts
@@ -7,22 +7,10 @@ interface Project {
 
 const openSourceData: Project[] = [
   {
-    title: 'Huggingface Hub',
-    description: `Hugging Face is the leading NLP company in the world. I have contributed to their open-source projects, especially the Huggingface Hub.`,
-    imgSrc: '/static/images/huggingface_hub.png',
-    href: 'https://github.com/huggingface/huggingface_hub/commits?author=lappemic',
-  },
-  {
     title: 'Kornia',
     description: `Kornia is a differentiable computer vision library for PyTorch. I am a maintainer of the project.`,
     imgSrc: '/static/images/kornia.png',
     href: 'https://github.com/kornia/kornia/commits?author=lappemic',
-  },
-  {
-    title: 'Gradio',
-    description: `Gradio is the fastest way to demo your machine learning model with a friendly web interface so that anyone can use it. I have contributed minor bug fixes.`,
-    imgSrc: '/static/images/gradio.png',
-    href: 'https://github.com/gradio-app/gradio/commits?author=lappemic',
   },
   {
     title: 'bank.green',
@@ -31,10 +19,22 @@ const openSourceData: Project[] = [
     href: 'https://bank.green/',
   },
   {
+    title: 'Huggingface Hub',
+    description: `Hugging Face is the leading NLP company in the world. I have contributed to their open-source projects, especially the Huggingface Hub.`,
+    imgSrc: '/static/images/huggingface_hub.png',
+    href: 'https://github.com/huggingface/huggingface_hub/commits?author=lappemic',
+  },
+  {
     title: 'Protontypes',
     description: `A Free Community Accelerating Open and Sustainable Technology. I am a member of the organization and contribute to various projects, mainly in the field of NLP.`,
     imgSrc: '/static/images/protontypes.png',
     href: 'https://github.com/orgs/protontypes/people',
+  },
+  {
+    title: 'Gradio',
+    description: `Gradio is the fastest way to demo your machine learning model with a friendly web interface so that anyone can use it. I have contributed minor bug fixes.`,
+    imgSrc: '/static/images/gradio.png',
+    href: 'https://github.com/gradio-app/gradio/commits?author=lappemic',
   },
 ]
 
