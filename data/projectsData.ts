67797fbbf2681fd2f11a86d1ad3c318334423a96
Michael Scheiwiller <mi.lappert@gmail.com>
2024-12-18 08:06:34 +0100

    refactor: update project order

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,24 +9,23 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'csv2ai.io',
-    description: `csv2ai takes a csv of product descriptions and optimizes the description and title, optimizes for SEO and lets you translate everything to 100+ languages. 
-    Built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Contentlayer](https://www.contentlayer.dev/) and [Supabase](https://supabase.com/).`,
-    imgSrc: '/static/images/csv2ai.png',
-    href: 'https://www.csv2ai.io?ref=michaelscheiwiller.com',
+    title: 'NextGenEnergyJobs.com',
+    description:
+      'A jobboard for renewable energy jobs. Built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Shadcn/UI](https://ui.shadcn.com/), [MongoDB](https://www.mongodb.com/) and [Algolia](https://www.algolia.com/).',
+    imgSrc: '/static/images/nextgenenergyjobs.png',
+    href: 'https://www.nextgenenergyjobs.com?ref=michaelscheiwiller.com',
+  },
+  {
+    title: 'OpenClimate.fund',
+    description: `A crowdfunding platform for climate projects. Built with [Next.js](https://nextjs.org/) on github pages.`,
+    imgSrc: '/static/images/openclimate.fund.png',
+    href: 'https://protontypes.github.io/open-climate-fund/?ref=michaelscheiwiller.com',
   },
   {
     title: 'SustainabilitySoftwares.com',
     description: `A directory of sustainability softwares. Built with [Next.js](https://nextjs.org/), [Contentlayer](https://www.contentlayer.dev/) and [Tailwind CSS](https://tailwindcss.com/).`,
     imgSrc: '/static/images/sustainabilitysoftwares.png',
     href: 'https://www.sustainabilitysoftwares.com?ref=michaelscheiwiller.com',
-  },
-  {
-    title: 'NextGenEnergyJobs.com',
-    description:
-      'A jobboard for renewable energy jobs. Built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Shadcn/UI](https://ui.shadcn.com/), [MongoDB](https://www.mongodb.com/) and [Algolia](https://www.algolia.com/).',
-    imgSrc: '/static/images/nextgenenergyjobs.png',
-    href: 'https://www.nextgenenergyjobs.com?ref=michaelscheiwiller.com',
   },
   {
     title: 'MindBoostingBooks.com',
@@ -49,10 +48,11 @@ const projectsData: Project[] = [
     href: 'https://www.officiallyrefurbished.com?ref=michaelscheiwiller.com',
   },
   {
-    title: 'OpenClimate.fund',
-    description: `A crowdfunding platform for climate projects. Built with [Next.js](https://nextjs.org/) on github pages.`,
-    imgSrc: '/static/images/openclimate.fund.png',
-    href: 'https://protontypes.github.io/open-climate-fund/?ref=michaelscheiwiller.com',
+    title: 'csv2ai.io',
+    description: `csv2ai takes a csv of product descriptions and optimizes the description and title, optimizes for SEO and lets you translate everything to 100+ languages. This is the result of a freelancing gig i did for a client.
+    Built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Contentlayer](https://www.contentlayer.dev/) and [Supabase](https://supabase.com/).`,
+    imgSrc: '/static/images/csv2ai.png',
+    href: 'https://www.csv2ai.io?ref=michaelscheiwiller.com',
   },
   {
     title: 'This Website :D',
