c8243d172b016006939cc3fafc358b4bfb362dbe
Michael Scheiwiller <mi.lappert@gmail.com>
2025-01-22 15:27:51 +0100

    refactor: add timlrx link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,9 +22,14 @@ export default function Footer() {
           <div>{siteMetadata.author}</div>
           {/* <Link href="/">{siteMetadata.title}</Link> */}
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 text-sm text-gray-500 dark:text-gray-400">
           <Link href="/">Home </Link> | <Link href="/projects"> Projects </Link> |{' '}
           <Link href="/blog"> Blog</Link>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+            Tailwind Nextjs Theme
+          </Link>
         </div>
       </div>
     </footer>
